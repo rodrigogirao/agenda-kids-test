9c44d4d7f3f1c73acf36c181430fed7eb1704b02
Rodrigo Girão <rodrigogirao7@gmail.com>
2017-01-08 10:51:38 -0300

    Added customizable login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      # root to: 'users#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+
+    devise_for :users
+  end
 end
